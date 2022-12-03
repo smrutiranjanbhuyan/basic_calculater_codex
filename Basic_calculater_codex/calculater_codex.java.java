8c116ce997a111245c85979607f5cf3bee0d9ef9
smrutiranjanbhuyan <118583676+smrutiranjanbhuyan@users.noreply.github.com>
2022-12-03 22:44:24 +0530

    Add files via upload

---
+++ Basic_calculater_codex/calculater_codex.java.java
@@ -0,0 +1,59 @@
+
+package smruti;
+import java.util.Scanner;
+
+public class calculater_codex {
+
+	public static void main(String[] args) {
+		char operater;
+		Double a,b,c;
+				Scanner Sc= new Scanner(System.in);
+				System.out.println("Input 1st nuber");
+				a=Sc.nextDouble();
+				System.out.println("Choose an oporater:+,-,*,/");
+				operater=Sc.next().charAt(0);
+				System.out.println("Input 2nd number");
+				b=Sc.nextDouble();
+			
+switch(operater) {
+case '+':
+	c=a+b;
+	System.out.println("Addition=");
+	System.out.println(a+b);
+	break;
+case '-':
+	c=a+b;
+	System.out.println("Subtractiom=");
+	System.out.println(a-b);
+	break;
+case '*':
+	c=a+b;
+	System.out.println("Multiplication=");
+	System.out.println(a*b);
+	break;
+case '/':
+	c=a+b;
+	System.out.println("Division=");
+	System.out.println(a/b);
+	break;
+	default:
+	System.out.println("invalid input");
+}
+			
+			
+				
+				
+				}
+				
+				
+			
+			
+
+
+		}
+
+	
+
+
+
+	
